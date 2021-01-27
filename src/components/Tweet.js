1a9de22435ae4d51f7463a498f6877365eea63b6
Bartosz Daniszewski <bdaniszewski@multic.pl>
2021-01-27 17:44:44 +0100

    Add the ability to like/unlike tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,9 @@ import { db } from "../firebase";
 const Tweet = ({ id, by, byId, body, handle }) => {
   const [commentBody, setCommentBody] = useState("");
   const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [liked, setLiked] = useState(false);
+  const [currentUserLikeId, setCurrentUserLikeId] = useState();
 
   const addComment = async () => {
     try {
@@ -23,6 +26,48 @@ const Tweet = ({ id, by, byId, body, handle }) => {
       ]);
     } catch {
       console.error("ERROR COMMENT");
+    }
+  };
+
+  const likeTweet = async () => {
+    try {
+      setLiked(true);
+      const ref = await db
+        .collection("users")
+        .doc(byId)
+        .collection("tweets")
+        .doc(id)
+        .collection("likes")
+        .add({
+          likedBy: handle,
+        });
+      setLikes([...likes, { id: ref.id, likedBy: handle }]);
+      setCurrentUserLikeId(ref.id);
+    } catch {
+      console.error("ERROR LIKE");
+      setLiked(false);
+    }
+  };
+
+  const unlikeTweet = async () => {
+    try {
+      setLiked(false);
+      await db
+        .collection("users")
+        .doc(byId)
+        .collection("tweets")
+        .doc(id)
+        .collection("likes")
+        .doc(currentUserLikeId)
+        .delete();
+      const updatedLikes = likes.filter((like) => like.likedBy != handle);
+      console.log("likes length", likes.length, handle);
+      console.log("updatgedLikes liength", updatedLikes.length);
+      setLikes(updatedLikes);
+    } catch {
+      console.error("ERROR LIKE");
+
+      setLiked(true);
     }
   };
 
@@ -48,13 +93,55 @@ const Tweet = ({ id, by, byId, body, handle }) => {
         });
       });
     };
+
+    const fetchLikes = async () => {
+      const data = await db
+        .collection("users")
+        .doc(byId)
+        .collection("tweets")
+        .doc(id)
+        .collection("likes")
+        .get();
+      data.forEach((doc) => {
+        setLiked(doc.data().likedBy == handle);
+        setCurrentUserLikeId(doc.id);
+        setLikes((prevState) => {
+          return [
+            ...prevState,
+            {
+              likedBy: doc.data().likedBy,
+              id: doc.id,
+            },
+          ];
+        });
+      });
+    };
+
     fetchComments();
+    fetchLikes();
   }, []);
 
   return (
     <div key={id} style={{ border: "2px solid blue", padding: "10px" }}>
       <strong>{by}</strong> said:
       <p>{body}</p>
+      <button onClick={liked ? unlikeTweet : likeTweet}>
+        <span>
+          {liked ? (
+            <span role="img" aria-label="heart">
+              💚
+            </span>
+          ) : (
+            <span role="img" aria-label="heart">
+              ❤
+            </span>
+          )}
+        </span>
+      </button>
+      <div>
+        {console.log(likes)}
+        <strong>{likes.length} </strong>likes
+      </div>
       <label htmlFor="comment">Add comment</label>
       <input
         id="comment"
